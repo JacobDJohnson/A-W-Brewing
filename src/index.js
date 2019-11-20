2d178b3a6412ef9ea1d2190e81712fbb72bb5593
Jacob Johnson <jacobdjohnson0831@gmail.com>
2019-11-19 19:04:01 -0600

    Set up for Git Pages

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Welcome to Brew Site.
+            Welcome to A&W Brewing
           </p>
         </header>
       </div>
